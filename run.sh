2a26d75b9223fea0337c4427c30d3d250945c2e9
Loggus66 <46504103+Loggus66@users.noreply.github.com>
2020-11-06 03:05:27 +0500

    Update run.sh
    Android 8.1 does not build with vndk v28. I propose a separate branch for that.

--- run.sh
+++ run.sh
@@ -21,12 +21,3 @@ for vndk in 26 27;do
 		rm -f "$t"
 	done
 done
-
-for vndk in 28 29;do
-	for src in $(find "$1"/sepolicies/$vndk -name \*sepolicy.cil);do
-		plat_pub="$(echo "$src" |sed -E 's/_sepolicy.cil$/_plat_pub.cil/g')"
-		./out/host/${TARGET_INCLUDE}-x86/bin/secilc "$2"/system/etc/selinux/plat_sepolicy.cil -o "$t" -M true -G -N -c 30 "$2"/system/etc/selinux/mapping/${vndk}.0.cil $sext "$plat_pub" "$src"
-		./out/host/${TARGET_INCLUDE}-x86/bin/checkpolicy -M -bC -o /dev/null "$t"
-		rm -f "$t"
-	done
-done
